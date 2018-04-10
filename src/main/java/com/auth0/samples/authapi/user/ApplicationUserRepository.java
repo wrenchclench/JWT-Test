52a269d9484444b0abbc960fe487357910b2a7bb
John Budreski <john.budreski@gmail.com>
2018-04-10 18:19:09 +0800

    JWT Test

--- src/main/java/com/auth0/samples/authapi/user/ApplicationUserRepository.java
+++ src/main/java/com/auth0/samples/authapi/user/ApplicationUserRepository.java
@@ -1,4 +1,7 @@
 package com.auth0.samples.authapi.user;
 
-public interface ApplicationUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+	ApplicationUser findByUsername(String username);
 }
